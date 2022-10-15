1458e8e4dde4a615fff4e1d666d8ac0e7a8f9e51
loai <loaiatabani@gmail.com>
2022-10-15 02:10:11 +0200

    add onborading and files

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -3,6 +3,7 @@ import 'package:eeese_hackathon/view/screens/auth/login.dart';
 import 'package:eeese_hackathon/view/screens/auth/profile_pic.dart';
 import 'package:eeese_hackathon/view/screens/auth/signup.dart';
 import 'package:eeese_hackathon/view/screens/events/events_screens.dart';
+import 'package:eeese_hackathon/view/screens/onboarding_screen.dart';
 import 'package:get/get.dart';
 
 class AppRouter {
@@ -11,18 +12,21 @@ class AppRouter {
   static const String auth = '/auth';
   static const String profilepic = '/profile_picture';
   static const String events = '/events';
+  static const String onboarding = '/onboarding';
 
   static String getLogin() => login;
   static String getSignup() => signup;
   static String getAuth() => auth;
   static String getProfilePic() => profilepic;
   static String getEvents() => events;
+  static String getOnboarding() => onboarding;
 
   static List<GetPage> routs = [
     GetPage(name: login, page: (() => const LoginScreen())),
     GetPage(name: signup, page: (() => SignUpScreen())),
     GetPage(name: auth, page: (() => const AuthScreen())),
-    GetPage(name: profilepic, page: (() => ProfilePictureScreen())),
+    GetPage(name: profilepic, page: (() => const ProfilePictureScreen())),
     GetPage(name: events, page: (() => const EventsScreen())),
+    GetPage(name: onboarding, page: (() => const OnboardingScreen())),
   ];
 }
